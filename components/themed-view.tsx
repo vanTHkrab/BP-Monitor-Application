2e3dcfad795c36c21cac1df3aeb6369423464e31
ok999p <tachanon.srisook@gmail.com>
2026-01-16 21:41:42 +0700

    BackUP

--- components/themed-view.tsx
+++ components/themed-view.tsx
@@ -1,14 +1,24 @@
 import { View, type ViewProps } from 'react-native';
+import { styled } from 'nativewind';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  className?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+const StyledView = styled(View);
+
+export function ThemedView({ style, lightColor, darkColor, className, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return (
+    <StyledView
+      className={className}
+      style={[{ backgroundColor }, style]}
+      {...otherProps}
+    />
+  );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativewind } = require("nativewind/metro");
+const { getDefaultConfig } = require('@expo/metro-config');
+const { withReactNativeCSS } = require('react-native-css/metro');
 
-
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativewind(config);
- 
+module.exports = withReactNativeCSS(config);
