92c1dcb45d873ee590433dcb64ee69601213fb5d
ok999p <tachanon.srisook@gmail.com>
2026-03-20 04:52:21 +0700

    Restructure project: move client and add server

--- app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Explore() {
-    return (
-        <View>
-            <Text>Explore</Text>
-        </View>
-    );
-}
--- components/hello-wave.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from 'react-native';
-
-export function HelloWave() {
-  return (
-    <Text className="text-[28px] leading-8 -mt-1.5">
-      👋
-    </Text>
-  );
-}
