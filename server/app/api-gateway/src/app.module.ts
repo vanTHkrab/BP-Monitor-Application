291b82835e01dbfb30d13542c78923cd714dd50e
vanTHkrab <vanth2548@gmail.com>
2026-03-28 16:36:07 +0700

    Edit server to run hybird service Change protocal http to graphql

--- server/app/api-gateway/src/app.module.ts
+++ server/app/api-gateway/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { join } from 'path';
+
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      graphiql: true,
+      subscription: true,
+    }),
+    PrismaModule,
+  ],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- server/app/api-gateway/src/main.ts
+++ server/app/api-gateway/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify/adapters/fastify-adapter';
+import type { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors();
+  }
+
+  const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+  await app.listen(PORT, '0.0.0.0');
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error starting API Gateway:', err);
+  process.exit(1);
+});
