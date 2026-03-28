af275c8a2650774ca006680c09f7c3e80cd3f50c
vanTHkrab <vanth2548@gmail.com>
2026-03-28 16:56:01 +0700

    Add simple transport redis

--- server/app/api-gateway/src/app.module.ts
+++ server/app/api-gateway/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import Redis from 'ioredis';
 import { join } from 'path';
 
 import { AppResolver } from './app.resolver';
@@ -15,8 +17,29 @@ import { PrismaModule } from './prisma/prisma.module';
       graphiql: true,
       subscription: true,
     }),
+    ClientsModule.register([
+      {
+        name: 'AI_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          port: 6379,
+        },
+      },
+    ]),
     PrismaModule,
   ],
-  providers: [AppService, AppResolver],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: () => {
+        return new Redis({
+          host: 'localhost',
+          port: 6379,
+        });
+      },
+    },
+    AppService,
+    AppResolver,
+  ],
 })
 export class AppModule {}
